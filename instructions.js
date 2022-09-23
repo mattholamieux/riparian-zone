b72ac6e1ed29d6a071f25e77956584e42ca7bd1e
mattholamieux <50525591+mattholamieux@users.noreply.github.com>
2022-09-23 13:37:33 -0700

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -4,6 +4,7 @@ const instructions = [
     "mouse y controls rate",
     "mouse x controls pitch",
     "click/drag to set loop points",
+    "(drag right to left to reverse playback)",
     "left & right arrows to change sample",
     "scroll up and down to change grain size"
 ];
