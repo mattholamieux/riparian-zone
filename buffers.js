8f046f75568cd6a189f68b5f0473b7cfe0dfcc7e
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2022-09-23 13:49:45 -0700

    add new samples

--- buffers.js
+++ buffers.js
@@ -1,6 +1,8 @@
 const buffer1 = new Tone.ToneAudioBuffer("samples/loop1.wav");
 const buffer2 = new Tone.ToneAudioBuffer("samples/loop2.wav");
 const buffer3 = new Tone.ToneAudioBuffer("samples/loop3.wav");
-const buffer4 = new Tone.ToneAudioBuffer("https://s3-us-west-1.amazonaws.com/leesamples/samples/Natural+Sounds/Birdsong.mp3");
-const buffer5 = new Tone.ToneAudioBuffer("samples/storms_never_last.mp3");
-const buffer6 = new Tone.ToneAudioBuffer("samples/midtown120.mp3");
+const buffer4 = new Tone.ToneAudioBuffer("samples/loop4.mp3");
+const buffer5 = new Tone.ToneAudioBuffer("samples/loop5.mp3");
+const buffer6 = new Tone.ToneAudioBuffer("https://s3-us-west-1.amazonaws.com/leesamples/samples/Natural+Sounds/Birdsong.mp3");
+const buffer7 = new Tone.ToneAudioBuffer("samples/storms_never_last.mp3");
+const buffer8 = new Tone.ToneAudioBuffer("samples/midtown120.mp3");
