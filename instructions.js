4339959f02f26cac49416a62f0126dca671e90da
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2023-03-06 21:44:10 -0800

    change audio files

--- instructions.js
+++ instructions.js
@@ -1,10 +1,8 @@
 const instructions = [
-    "P5_GRAIN:",
     "space to start/stop",
     "mouse y controls rate",
     "mouse x controls pitch",
     "click/drag to set loop points",
-    "(drag right to left to reverse playback)",
     "left & right arrows to change sample",
     "scroll up and down to change grain size"
 ];
--- sketch.js
+++ sketch.js
@@ -6,12 +6,14 @@ let x1 = 0,
     y2 = 0;
 const bgColors = [100, 150, 250];
 let bgIndex = 0;
-const buffers = [buffer1, buffer2, buffer3, buffer4, buffer5, buffer6, buffer7, buffer8, buffer9]; // Defined in buffers.js
+const buffers = [buffer1, buffer2, buffer3, buffer4, buffer5, buffer6, buffer7, buffer8]; // Defined in buffers.js
 let bufferIndex = 0;
 let loopStart, loopEnd, pressedPoint, releasePoint;
 let isPlaying = false;
 let grainSize = 0.1;
 let overlap = 0.1;
+let angleRotate = 0;
+let t = 0;
 
 // Instantiate Tone.GrainPlayer object
 const player = new Tone.GrainPlayer(buffers[bufferIndex]);
@@ -39,47 +41,71 @@ function setup() {
     player.grainSize = grainSize;
     reverb.toDestination();
     player.chain(reverb);
-
+    player.loopStart = 0;
+    player.loopEnd = buffers[bufferIndex].duration;
     pressedPoint = 0;
     releasePoint = 1;
 }
 
 function draw() {
-    // Draw background
-    if (mouseX < width && mouseX > 0 && mouseY < height && mouseY > 0) {
-        background(mouseX / 2, mouseY / 2, bgColors[bgIndex]);
+
+    if (player.loaded) {
+
+        // Draw background
+        if (mouseX < width && mouseX > 0 && mouseY < height && mouseY > 0) {
+            background(mouseX / 2, mouseY / 2, bgColors[bgIndex], 5);
+        }
+        // Draw instructions
+        noStroke();
+        let f = 80;
+        textSize(20);
+        textLeading(5);
+        textFont("serif");
+        for (i = 0; i < instructions.length; i++) {
+            fill(f);
+            text(instructions[i], 10, 20 * [i] + 20);
+            f += 10;
+        }
+
+        // Draw rect to indicate loop start and end
+        rectMode(CORNER);
+        fill(40, 54, 100, 30, 50);
+        rect(x1, 0, x2 - x1, height);
+
+        // Draw rect to represent grain size
+        let grainSizeDisplay = map(grainSize, 0.01, 2, (width / 80), (height / 1.5));
+        let overlapDisplay = map(overlap, 0.01, 2, (width / 80), (height / 1.5));
+        rectMode(CENTER);
+        fill(155, 0, 0, 5);
+        rect(width / 2, height / 2, overlapDisplay, grainSizeDisplay);
+
+        push();
+        textSize(30);
+        translate(width / 2, height / 2);
+        rotate(radians(angleRotate));
+        fill(50);
+        // fill(0, 204, 255, 50);
+        // stroke(255, 204, 0, 50)
+        strokeWeight(1);
+        text("Type/Token: Riparian Zone", 0, 0);
+        pop();
+        angleRotate += 0.25;
+
+        // Affect player's tune and rate with mouseX and mouseY
+        if (mouseX < width && mouseX > 0) {
+            player.detune = (mouseX / (width / 4)) * 1200 - 2400;
+        }
+        if (mouseY < height && mouseY > 0) {
+            player.playbackRate = mouseY / (height / 2) + 0.05;
+        }
     }
 
-    // Draw instructions
-    noStroke();
-    let f = 80;
-    textSize(30);
-    textLeading(30);
-    textFont("serif");
-    for (i = 0; i < instructions.length; i++) {
-        fill(f);
-        text(instructions[i], 10, 30 * [i] + 30);
-        f += 10;
-    }
+    // else {
+    //  Loading Animation Here
 
-    // Draw rect to indicate loop start and end
-    rectMode(CORNER);
-    fill(40, 54, 100, 30);
-    rect(x1, 0, x2 - x1, height);
+    // }
 
-    // Draw rect to represent grain size
-    let grainSizeDisplay = map(grainSize, 0.01, 2, (width / 80), (height / 1.5));
-    rectMode(CENTER);
-    fill(255, 204, 0, 50);
-    rect(width / 2, height / 2, grainSizeDisplay, grainSizeDisplay);
 
-    // Affect player's tune and rate with mouseX and mouseY
-    if (mouseX < width && mouseX > 0) {
-        player.detune = (mouseX / (width / 4)) * 1200 - 2400;
-    }
-    if (mouseY < height && mouseY > 0) {
-        player.playbackRate = mouseY / (height / 2) + 0.05;
-    }
 }
 
 function getPressedPoint() {
@@ -87,6 +113,7 @@ function getPressedPoint() {
     pressedPoint = mouseX / width;
     x1 = mouseX;
     y1 = mouseY;
+    player.stop();
 }
 
 function getReleasePoint() {
@@ -102,18 +129,18 @@ function calculateLoop() {
     // Calculate loop start and end points in relation to current buffer's duration
     loopStart = pressedPoint * buffers[bufferIndex].duration;
     loopEnd = releasePoint * buffers[bufferIndex].duration;
-
     // If mouse dragged left to right, play forwards
     if (loopStart < loopEnd) {
         player.loopStart = loopStart;
         player.loopEnd = loopEnd;
         player.reverse = false;
-        player.start(1, loopStart);
+        let bloopStart = loopEnd - 0.1
+        player.sync().start("+0.5", loopStart);
     } else { // otherwise, play backwards
         player.loopStart = loopEnd;
         player.loopEnd = loopStart;
         player.reverse = true;
-        player.start(1, loopEnd);
+        player.sync().start("+0.5", loopEnd);
     }
     isPlaying = true;
 }
@@ -123,7 +150,7 @@ function keyPressed() {
     if (key === " ") {
         if (!isPlaying) {
             initializeTone();
-            player.start(1, loopStart);
+            player.sync().start("+0.5", loopStart);
             isPlaying = true;
         } else {
             player.stop();
@@ -164,12 +191,24 @@ function trackPad(event) {
         if (grainSize < 2) {
             grainSize += 0.01;
         }
+    } else if (event.wheelDeltaX > 10) {
+        if (overlap < 2) {
+            overlap += 0.01;
+        }
+    } else if (event.wheelDeltaX < -10) {
+        if (overlap > 0.01) {
+            overlap -= 0.01;
+        }
     }
     player.grainSize = grainSize;
+    player.overlap = overlap;
+
+
 }
 
 async function initializeTone() {
     await Tone.start();
+    Tone.Transport.start();
     console.log("audio context started");
 }
 
