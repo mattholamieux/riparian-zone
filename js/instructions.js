6cb048fcdcd8587768cbaeb8edee6db0c6b426de
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2023-09-09 12:11:02 -0700

    update instruction display

--- js/instructions.js
+++ js/instructions.js
@@ -1,26 +1,35 @@
 const mainInstructions = [
     "space to start/stop",
     "click + drag ↔ to set loop points",
-    "(dragging right to left plays in reverse)",
     "left & right arrows change tracks",
+    "a-l record looped delay buffers",
     "1, 2, 3, 4 for options"
 ];
 
 const secondaryInstructions = [
-    "mouse x : pitch",
-    "mouse y : rate",
-    "scroll ↕ : grain size",
-    "scroll ↔ : grain overlap",
-    "mouse x : filter frequency",
-    "mouse y : filter resonance",
-    "scroll ↕ : bitcrush amount",
-    "scroll ↔ : bit depth",
-    "mouse x : delay time",
-    "mouse y : delay feedback",
-    "scroll ↕ : delay amount",
-    "scroll ↔ : reverb amount",
-    "scroll ↕ : volume",
-    "scroll ↔ : pan",
-    " ",
-    " "
+    [
+        "mouse x : pitch",
+        "mouse y : rate",
+        "scroll ↕ : grain size",
+        "scroll ↔ : grain overlap",
+        "shift locks pitch to octaves"
+    ],
+    [
+        "mouse x : filter frequency",
+        "mouse y : filter resonance",
+        "scroll ↕ : bitcrush amount",
+        "scroll ↔ : bit depth",
+    ],
+    [
+        "mouse x : delay time",
+        "mouse y : delay feedback",
+        "scroll ↕ : delay amount",
+        "scroll ↔ : reverb amount",
+    ],
+    [
+        "scroll ↕ : volume",
+        "scroll ↔ : pan",
+        " ",
+        " "
+    ]
 ]
